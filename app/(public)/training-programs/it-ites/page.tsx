4154f486283b83f6117f6059873a8b30be40a780
creektechno24 <creektechno24@gmail.com>
2026-06-18 15:51:52 +0800

    it-ites updated in training programs

---
+++ app/(public)/training-programs/it-ites/page.tsx
@@ -0,0 +1,13 @@
+export default function ITITESPage() {
+  return (
+    <div className="container mx-auto px-6 py-20">
+      <h1 className="text-4xl font-bold mb-4">
+        IT & ITES Training Programs
+      </h1>
+
+      <p className="text-gray-600">
+        IT & ITES training programs will be available soon.
+      </p>
+    </div>
+  )
+}
